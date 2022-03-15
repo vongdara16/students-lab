7f2ac0c304a04234949bef0d080c5acc3f1ccfed
Justin Vongdara <vongdara16@gmail.com>
2022-03-15 14:07:16 -0400

    complete bonus

--- src/Score.jsx
+++ src/Score.jsx
@@ -2,8 +2,8 @@ const Score = (props) => {
   return (  
     <div>
       {/* test score comp */}
-      <h4>Test Date: {props.score.date}</h4>
-      <h5>Score: {props.score.score}</h5>
+      <h5>Test Date: {props.score.date}</h5>
+      <h6>Score: {props.score.score}</h6>
     </div>
   );
 }
